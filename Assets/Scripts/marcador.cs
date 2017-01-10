c56bcae7021c9ff6a36fbd22d4cf608a95129ab5
María Rubia Esteban <mariarubite7@gmail.com>
2017-01-10 20:43:50 +0100

    Examen

---
+++ Assets/Scripts/marcador.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class marcador : MonoBehaviour {
+	public Text texto_marcador;
+	int goles = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (goles >= 1) {
+			SceneManager.LoadScene ("nave");
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D col){
+		if (col.transform.name == "balon") {
+			goles += 1; //goles = goles +1
+			texto_marcador.text = goles + "";
+		}
+	}
+}
---
+++ Assets/Scripts/movimientoJ2.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movimientoJ2 : MonoBehaviour {
+	Rigidbody2D rb;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.W)) {
+			rb.velocity = new Vector2 (0, 10);
+		}
+		if (Input.GetKeyDown (KeyCode.S)) {
+			rb.velocity = new Vector2 (0, -10);
+		}
+		if (Input.GetKeyDown (KeyCode.A)) {
+			rb.velocity = new Vector2 (-10, 0);
+		}
+		if (Input.GetKeyDown (KeyCode.D)) {
+			rb.velocity = new Vector2 (10, 0);
+		}
+		//---------------GETKEYUP---------------------
+
+		if (Input.GetKeyUp (KeyCode.W)) {
+			rb.velocity = new Vector2 (0, 0);
+		}
+		if (Input.GetKeyUp (KeyCode.S)) {
+			rb.velocity = new Vector2 (0, 0);
+		}
+		if (Input.GetKeyUp (KeyCode.A)) {
+			rb.velocity = new Vector2 (0, 0);
+		}
+		if (Input.GetKeyUp (KeyCode.D)) {
+			rb.velocity = new Vector2 (0, 0);
+		}
+	}
+}
