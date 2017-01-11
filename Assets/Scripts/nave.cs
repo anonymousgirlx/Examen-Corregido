a9c70c212ff1d46ce92de11ffabc92ee66fcda16
María Rubia Esteban <mariarubite7@gmail.com>
2017-01-11 19:01:05 +0100

    Ejercicio Nave

---
+++ Assets/Scripts/nave.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class nave : MonoBehaviour {
+	public float fuerza = 10f;
+	public float rotation = 0.5f;
+
+	Rigidbody2D rb;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.Space)) {
+			rb.AddForce (transform.up * fuerza);
+		}
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			rb.AddTorque (rotation);
+		}
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			rb.AddTorque (-rotation);
+		}
+	}
+}
