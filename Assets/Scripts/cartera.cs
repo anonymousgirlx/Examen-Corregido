ea6b4ff86f3536a9e8a7866d53d2c08f0318de30
María Rubia Esteban <mariarubite7@gmail.com>
2017-01-13 17:58:08 +0100

    Ejercicio Robot / Fantasma

---
+++ Assets/Scripts/cartera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class cartera : MonoBehaviour {
+
+	int monedas = 100; // int = No se puede acceder a la variable desde fuera.
+	public Text texto_monedas;
+
+	void Update(){
+		texto_monedas.text = monedas.ToString ();
+	}
+
+	public void donacion (int dinero){
+		//if (dinero > 0) { Para asegurarnos que no puede robarle dinero.s
+			monedas += dinero;
+		//}
+	}
+}
---
+++ Assets/Scripts/ladron_script.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ladron_script : MonoBehaviour {
+	GameObject pringao;
+	cartera cartera_pringao;
+	// Use this for initialization
+	void Start () {
+		pringao = GameObject.Find ("ricardito");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown (KeyCode.Space)) {
+			cartera_pringao = pringao.GetComponent<cartera> ();
+			cartera_pringao.donacion (-5);
+		}
+	}
+}
